9862e7931bf291269a72bd3a4e74986c76f09f9d
beowulf11 <capkovicdenis@gmail.com>
2021-04-17 19:36:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import ScriptTag from "react-script-tag";
+
+function App() {
+  return (
+    <div>
+      <div
+        className="polis"
+        data-conversation_id="6dy7mz2wzv"
+        data-ui_lang="sk"
+      ></div>
+      <ScriptTag async type="text/javascript" src="https://pol.is/embed.js" />
+    </div>
+  );
+}
+
+export default App;
